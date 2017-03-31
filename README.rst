f3b32a86d6cfab35412a9ba907ad7c70e6edf4ae
Scott Wales <scott.wales@unimelb.edu.au>
2017-03-31 11:45:27 +1100

    Setup is running antlr

---
+++ README.rst
@@ -0,0 +1,38 @@
+=============================
+umdpfmt
+=============================
+
+UMDP3 Formatter
+
+.. image:: https://readthedocs.org/projects/umdpfmt/badge/?version=latest
+  :target: https://readthedocs.org/projects/umdpfmt/?badge=latest
+.. image:: https://travis-ci.org/ScottWales/umdpfmt.svg?branch=master
+  :target: https://travis-ci.org/ScottWales/umdpfmt
+.. image:: http://codecov.io/github/ScottWales/umdpfmt/coverage.svg?branch=master
+  :target: http://codecov.io/github/ScottWales/umdpfmt?branch=master
+.. image:: https://landscape.io/github/ScottWales/umdpfmt/master/landscape.svg?style=flat
+  :target: https://landscape.io/github/ScottWales/umdpfmt/master
+.. image:: https://codeclimate.com/github/ScottWales/umdpfmt/badges/gpa.svg
+  :target: https://codeclimate.com/github/ScottWales/umdpfmt
+.. image:: https://badge.fury.io/py/umdpfmt.svg
+  :target: https://pypi.python.org/pypi/umdpfmt
+
+.. content-marker-for-sphinx
+
+------------------
+Build Instructions
+------------------
+
+Pip install (into a virtual environment)::
+
+    pip install umdpfmt
+
+or::
+    
+    pip install git+https://github.com/ScottWales/umdpfmt
+
+or (for development)::
+
+    git checkout git+https://github.com/ScottWales/umdpfmt
+    cd umdpfmt
+    pip install -e '.[dev]'
---
+++ umdpfmt/_setup_hooks.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# Copyright 2017 ARC Centre of Excellence for Climate Systems Science
+# author: Scott Wales <scott.wales@unimelb.edu.au>
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import print_function
+
+import sys
+import setuptools
+import subprocess
+
+def antlr_gen(config):
+    target = '-Dlanguage=Python%d'%(sys.version_info[0])
+    command = ['java', '-jar', 'antlr-4.7-complete.jar', \
+            target, 'umdpfmt/parser/fortran.g4']
+    subprocess.check_call(command)
+
+
+class AntlrGen(setuptools.Command):
+    description = "Generate Antlr parser"
+    command_name = "antlrgen"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        antlr_gen(None)
+
